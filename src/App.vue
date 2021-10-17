c15fa3b38a842b2c7f1b27ab7cd4f927d8a0c00c
Yun Zhi Lin <yun@yunspace.com>
2021-10-17 23:32:04 +1100

    added Cognito authentication

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <div id="app">
-    <h1>Todo App</h1>
-    <input type="text" v-model="name" placeholder="Todo name">
-    <input type="text" v-model="description" placeholder="Todo description">
-    <button v-on:click="createTodo">Create Todo</button>
-  </div>
+  <amplify-authenticator>
+    <div id="app">
+      <h1>Todo App</h1>
+      <input type="text" v-model="name" placeholder="Todo name">
+      <input type="text" v-model="description" placeholder="Todo description">
+      <button v-on:click="createTodo">Create Todo</button>
+    </div>
+    <amplify-sign-out></amplify-sign-out>
+  </amplify-authenticator>
 </template>
 
 <script>
 import { API } from 'aws-amplify';
 import { createTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
 
 export default {
   name: 'app',
